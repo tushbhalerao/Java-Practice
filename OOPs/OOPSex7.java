3ceb5c07ee6528e93ed80b03f96d3356da1c67ec
tushbhalerao <tushbhalerao@gmail.com>
2021-05-27 22:55:34 +0530

    add OOPSex7.java

---
+++ OOPs/OOPSex7.java
@@ -0,0 +1,18 @@
+/*Q. toString method internally or externally
+A@5c8da962
+A@5c8da962
+A@5c8da962
+*/
+class A{
+    
+}
+public class OOPSex7 {
+    public static void main(String args[]) {
+    A a= new A();
+    String ref=a.toString();
+    System.out.println(ref);
+    System.out.println(a.toString());
+    System.out.println(a);
+    
+    }
+}
